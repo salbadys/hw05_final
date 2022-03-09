6879fd45374e7548583c8cba8d5b3094969a1176
Александр Баденков <salbadys@gmail.com>
2022-03-09 20:42:20 +0700

    Финальное задание(4)

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,3 @@
-from django.core.cache import cache
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from django.views.decorators.cache import cache_page
